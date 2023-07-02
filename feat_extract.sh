ebad0114430c773b49c1bc04227e2fd5f56ddeef
官宝 梁 <2031808280@qq.com>
2023-07-02 10:58:11 +0800

    datastore update

--- feat_extract.sh
+++ feat_extract.sh
@@ -1,3 +1,4 @@
 python src/dataprocess/0_process_user_activity_logs.py
 python src/dataprocess/1_process_user_contextual.py
 python src/dataprocess/2_table_2_numpy.py
+python src/dataprocess/3_mapping.py
