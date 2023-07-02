bdcd1382d29214e64cd4e9331ce7c017595d69e5
官宝 梁 <2031808280@qq.com>
2023-07-02 14:54:50 +0800

    graph

--- feat_extract.sh
+++ feat_extract.sh
@@ -2,3 +2,6 @@ python src/dataprocess/0_process_user_activity_logs.py
 python src/dataprocess/1_process_user_contextual.py
 python src/dataprocess/2_table_2_numpy.py
 python src/dataprocess/3_mapping.py
+python src/linkprediction/linkprediction_models.py
+python src/graphgeneration/0_create_strong_classmates_graph.py
+python src/graphgeneration/1_graph_addition_information.py
